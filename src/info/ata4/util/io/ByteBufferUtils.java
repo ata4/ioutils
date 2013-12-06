6f367a0835525a2af3f230411a45cfe4e8b45573
ata4 <barracuda415@yahoo.de>
2013-12-06 14:55:32 +0100

    Fixed some legit cases where an empty byte buffer is expected

--- src/info/ata4/util/io/ByteBufferUtils.java
+++ src/info/ata4/util/io/ByteBufferUtils.java
@@ -17,11 +17,18 @@ import java.nio.ByteBuffer;
  * @author Nico Bergemann <barracuda415 at yahoo.de>
  */
 public class ByteBufferUtils {
+    
+    private static final ByteBuffer EMPTY = ByteBuffer.allocate(0);
 
     private ByteBufferUtils() {
     }
     
     public static ByteBuffer getSlice(ByteBuffer bb, int offset, int length) {
+        if (length == 0) {
+            // very funny
+            return EMPTY;
+        }
+        
         // get current position and limit
         int pos = bb.position();
         int limit = bb.limit();
@@ -44,6 +51,6 @@ public class ByteBufferUtils {
     }
     
     public static ByteBuffer getSlice(ByteBuffer bb, int offset) {
-        return getSlice(bb, offset, 0);
+        return getSlice(bb, offset, -1);
     }
 }
