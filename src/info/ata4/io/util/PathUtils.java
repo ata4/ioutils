d404f65a38bf1a599318655315e15316a4de4c7b
ata4 <barracuda415@yahoo.de>
2014-09-24 15:40:25 +0200

    Added getBaseName()

--- src/info/ata4/io/util/PathUtils.java
+++ src/info/ata4/io/util/PathUtils.java
@@ -25,6 +25,10 @@ public class PathUtils {
     private PathUtils() {
     }
     
+    public static String getBaseName(Path path) {
+        return FilenameUtils.getBaseName(path.getFileName().toString());
+    }
+    
     public static String getExtension(Path path) {
         return FilenameUtils.getExtension(path.getFileName().toString());
     }
